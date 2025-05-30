d03a492da4c503bd5ad80728af50e86ac742a942
jijysun <jijysun@naver.com>
2025-05-30 20:59:20 +0900

    set Spring Security Config

---
+++ PracticeJPA/src/main/java/HelloJPA/PracticeJPA/controller/MemberViewController.java
@@ -0,0 +1,32 @@
+package HelloJPA.PracticeJPA.controller;
+
+import HelloJPA.PracticeJPA.dto.member.MemberRequestDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberViewController {
+
+    @PostMapping ("login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage(Model model) {
+        model.addAttribute("memberJoinDTO", new MemberRequestDto.JoinDto());
+        return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home (){
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
